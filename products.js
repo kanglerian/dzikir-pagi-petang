1f4da1fce969d1608ec08c2f8f9c2a91c1fa858e
Lerian <kanglerian@gmail.com>
2026-07-11 09:25:42 +0700

    baru

--- products.js
+++ products.js
@@ -7,10 +7,38 @@ const productsData = [
     shopeeUrl: "https://s.shopee.co.id/2LWc0JAQY4"
   },
   {
+    name: "Buku Doa dan Wirid Penulis Yazid bin Abdul Qadir Jawas - Pustaka Imam Syafi'i",
+    category: "Buku",
+    image: "./products/doa-dan-wirid.webp",
+    description: "Buku doa dan wirid yang ditulis oleh Yazid bin Abdul Qadir Jawas dan diterbitkan oleh Pustaka Imam Syafi'i.",
+    shopeeUrl: "https://s.shopee.co.id/9KgMMdVIES"
+  },
+  {
     name: "Sajadah Arizona Qubah Ukuran 60x110cm Busa Empuk Motif Minimalis Anti Slip",
     category: "Aksesoris",
     image: "./products/sajadah-arizona-qubah-ukuran-60x110cm-busa-empuk-motif-minimalis-anti-slip.webp",
     description: "SajadahArizona Qubah Ukuran 60x110cm Busa Empuk Motif Minimalis Anti Slip.",
     shopeeUrl: "https://s.shopee.co.id/20tlbu7Tad"
+  },
+  {
+    name: "Nutrifarm Madu Hutan 500 ml / 736 gr",
+    category: "Herbal",
+    image: "./products/nutrifarm-madu-hutan-500-ml-736-gr.webp",
+    description: "Nutrifarm Madu Hutan 500 ml / 736 gr.",
+    shopeeUrl: "https://s.shopee.co.id/7KvHyUJY4l"
+  },
+  {
+    name: "STAZBIYA - Mukena TSABINA Series (Mukena Dewasa Motif Floral Bahan Rayon 2 in 1 Set Sajadah)",
+    category: "Aksesoris",
+    image: "./products/stazbiya-mukena-tsabina-series.webp",
+    description: "STAZBIYA - Mukena TSABINA Series (Mukena Dewasa Motif Floral Bahan Rayon 2 in 1 Set Sajadah).",
+    shopeeUrl: "https://s.shopee.co.id/9KgMMSV3tU"
+  },
+  {
+    name: "BACENO MUSLIM Peci Rajut Mercan Benang Poly Ukuran Dewasa",
+    category: "Aksesoris",
+    image: "./products/peci-rajut-mercan.webp",
+    description: "BACENO MUSLIM Peci Rajut Mercan Benang Poly Ukuran Dewasa.",
+    shopeeUrl: "https://s.shopee.co.id/111ERDlDvC"
   },
 ];
