09e07b97b52c3932416c4b057d7eb65720a442d3
Lerian <kanglerian@gmail.com>
2026-07-11 09:07:32 +0700

    sajadah

--- products.js
+++ products.js
@@ -1,52 +1,16 @@
 const productsData = [
   {
-    id: 1,
-    name: "Buku Saku Hisnul Muslim",
+    name: "Buku Dzikir Pagi dan Petang Penulis Yazid bin Abdul Qadir Jawas - Pustaka Imam Syafi'i",
     category: "Buku",
-    price: 15000,
-    priceDisplay: "Rp 15.000",
-    image: "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?w=500&auto=format&fit=crop&q=60",
-    description: "Kumpulan doa dan dzikir shahih lengkap berdasar Quran dan Sunnah.",
-    shopeeUrl: "https://shopee.co.id/search?keyword=buku%20saku%20hisnul%20muslim"
+    image: "./products/dzikir-pagi-dan-petang-yazid-bin-abdul-qadir-jawas.webp",
+    description: "Buku dzikir pagi dan petang yang ditulis oleh Yazid bin Abdul Qadir Jawas dan diterbitkan oleh Pustaka Imam Syafi'i.",
+    shopeeUrl: "https://s.shopee.co.id/2LWc0JAQY4"
   },
   {
-    id: 2,
-    name: "Digital Tasbih Ring",
-    category: "Aksesoris",
-    price: 25000,
-    priceDisplay: "Rp 25.000",
-    image: "https://images.unsplash.com/photo-1596462502278-27bfdc403348?w=500&auto=format&fit=crop&q=60",
-    description: "Counter dzikir mini digital yang nyaman melingkar di jari Anda.",
-    shopeeUrl: "https://shopee.co.id/search?keyword=tasbih%20digital"
+    name: "Sajadah Arizona Qubah Ukuran 60x110cm Busa Empuk Motif Minimalis Anti Slip",
+    category: "Aksesoris Muslim",
+    image: "./products/sajadah-arizona-qubah-ukuran-60x110cm-busa-empuk-motif-minimalis-anti-slip.webp",
+    description: "SajadahArizona Qubah Ukuran 60x110cm Busa Empuk Motif Minimalis Anti Slip.",
+    shopeeUrl: "https://s.shopee.co.id/20tlbu7Tad"
   },
-  {
-    id: 3,
-    name: "Sajadah Travel Praktis",
-    category: "Aksesoris",
-    price: 65000,
-    priceDisplay: "Rp 65.000",
-    image: "https://images.unsplash.com/photo-1606744824163-985d376605aa?w=500&auto=format&fit=crop&q=60",
-    description: "Sajadah travel anti-air, ringan, ringkas, dan mudah dibersihkan.",
-    shopeeUrl: "https://shopee.co.id/search?keyword=sajadah%20travel"
-  },
-  {
-    id: 4,
-    name: "Minyak Habbatussauda",
-    category: "Herbal",
-    price: 45000,
-    priceDisplay: "Rp 45.000",
-    image: "https://images.unsplash.com/photo-1512290923902-8a9f81dc236c?w=500&auto=format&fit=crop&q=60",
-    description: "Minyak jintan hitam murni berkhasiat menjaga imunitas tubuh.",
-    shopeeUrl: "https://shopee.co.id/search?keyword=habbatussauda%20oil"
-  },
-  {
-    id: 5,
-    name: "Madu Murni Al-Syifa 250g",
-    category: "Herbal",
-    price: 85000,
-    priceDisplay: "Rp 85.000",
-    image: "https://images.unsplash.com/photo-1587049352846-4a222e784d38?w=500&auto=format&fit=crop&q=60",
-    description: "Madu asli arab berkualitas tinggi kaya nutrisi.",
-    shopeeUrl: "https://shopee.co.id/search?keyword=madu%20al%20syifa"
-  }
 ];
